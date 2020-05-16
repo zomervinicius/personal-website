9f5d3c59894e1cd97b5207829cbb78e6d0cdacf3
zomervinicius <vinicius.zomer123@gmail.com>
2020-05-16 10:37:08 -0300

    add fields as required

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,44 +1,57 @@
+import { graphql } from "gatsby"
 import React from "react"
 import Helmet from "react-helmet"
-import { graphql } from 'gatsby'
 import Layout from "../components/layout"
 
-const ContactPage = ({
-  data: {
-    site
-  },
-}) => {
+const ContactPage = ({ data: { site } }) => {
   return (
     <Layout>
       <Helmet>
         <title>Contact — {site.siteMetadata.title}</title>
-        <meta name="description" content={"Contact page of " + site.siteMetadata.description} />
+        <meta
+          name="description"
+          content={"Contact page of " + site.siteMetadata.description}
+        />
       </Helmet>
       <div className="two-grids -contact">
-        <div className="post-thumbnail" style={{backgroundImage: `url('/assets/alexander-andrews-HgUDpaGPTEA-unsplash.jpg')`, marginBottom: 0}}>
+        <div
+          className="post-thumbnail"
+          style={{
+            backgroundImage: `url('/assets/alexander-andrews-HgUDpaGPTEA-unsplash.jpg')`,
+            marginBottom: 0,
+          }}
+        >
           <h1 className="post-title">Get in Touch</h1>
           <p>Let me help you kick start your next project &rarr;</p>
         </div>
         <div>
-          <form className="form-container" action="https://sendmail.w3layouts.com/SubmitContactForm" method="post">
+          <form
+            className="form-container"
+            action="https://sendmail.w3layouts.com/SubmitContactForm"
+            method="post"
+          >
             <div>
               <label htmlFor="w3lName">Name</label>
-              <input type="text" name="w3lName" id="w3lName"/>
+              <input type="text" name="w3lName" id="w3lName" required />
             </div>
             <div>
               <label htmlFor="w3lSender">Email</label>
-              <input type="email" name="w3lSender" id="w3lSender"/>
+              <input type="email" name="w3lSender" id="w3lSender" required />
             </div>
             <div>
               <label htmlFor="w3lSubject">Subject</label>
-              <input type="text" name="w3lSubject" id="w3lSubject"/>
+              <input type="text" name="w3lSubject" id="w3lSubject" required />
             </div>
             <div>
               <label htmlFor="w3lMessage">Message</label>
-              <textarea name="w3lMessage" id="w3lMessage"></textarea>
+              <textarea name="w3lMessage" id="w3lMessage" required></textarea>
             </div>
-            <div style={{display: "flex", justifyContent: "flex-end"}}>
-              <input type="submit" className="button -primary" style={{marginRight: 0}} />
+            <div style={{ display: "flex", justifyContent: "flex-end" }}>
+              <input
+                type="submit"
+                className="button -primary"
+                style={{ marginRight: 0 }}
+              />
             </div>
           </form>
         </div>
@@ -48,7 +61,7 @@ const ContactPage = ({
 }
 export default ContactPage
 export const pageQuery = graphql`
-  query ContactPageQuery{
+  query ContactPageQuery {
     site {
       siteMetadata {
         title
